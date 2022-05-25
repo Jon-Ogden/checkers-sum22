44ea4463fad114d0af9cfb6a7b89b8fc96e98929
Jon_Ogden <jogden1863@gmail.com>
2022-05-25 16:58:22 -0500

    grid

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import './App.css';
+import Row from './assets/row'
 
 function App() {
-  return (
+  const data = [
+    [' ', 'X', ' ', 'X', ' ', 'X', ' ', 'X'],
+    ['X', ' ', 'X', ' ', 'X', ' ', 'X', ' '],
+    [' ', 'X', ' ', 'X', ' ', 'X', ' ', 'X'],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    ['O', ' ', 'O', ' ', 'O', ' ', 'O', ' '],
+    [' ', 'O', ' ', 'O', ' ', 'O', ' ', 'O'],
+    ['O', ' ', 'O', ' ', 'O', ' ', 'O', ' ']
+  ]
 
+  return (
+    <table className='no-border'>
+      <tbody>
+      {data.map((rowData, index) => {
+          const number = data.length - index
+          
+          return <Row key={number.toString()} number={number} data={rowData} />
+        })}
+      </tbody>
+    </table>
   );
 }
 
