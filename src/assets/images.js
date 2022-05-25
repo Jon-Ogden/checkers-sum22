3102e6ad445d66bf495f7ff476f00ab8ec6dbc4a
Jon_Ogden <jogden1863@gmail.com>
2022-05-25 17:55:07 -0500

    added images

---
+++ src/assets/images.js
@@ -0,0 +1,44 @@
+const img1 = <img src="https://photos.smugmug.com/1x1-photos/i-LDnB38K/0/cec1cf4d/M/OGD_0091-M.jpg" alt="could not load"/>
+const img2 =  <img src="https://photos.smugmug.com/1x1-photos/i-CdWzgsh/0/6cbff63c/M/OGD_0034-M.jpg" alt="could not load"/>
+const img3 = <img src="https://photos.smugmug.com/1x1-photos/i-4gD2KGL/0/2ead2860/M/20210518_210654-M.jpg" alt="could not load"/>
+const img4 = <img src="https://photos.smugmug.com/1x1-photos/i-RBmnJNz/0/2ef82437/M/OGD_0380-M.jpg" alt="could not load"/>
+const img5 = <img src="https://photos.smugmug.com/1x1-photos/i-bJBzLWm/0/15081dfe/M/OGD_0238-M.jpg" alt="could not load"/>
+
+const img6 = <img src="https://photos.smugmug.com/1x1-photos/i-2zh6vNq/0/38a6ad0d/M/OGD_0156-M.jpg" alt="could not load"/>
+const img7 =  <img src="https://photos.smugmug.com/1x1-photos/i-bfzpvXs/0/1979ac06/M/OGD_0194-M.jpg" alt="could not load"/>
+const img8 = <img src="https://photos.smugmug.com/1x1-photos/i-jSSXQjW/0/5b9faf91/M/OGD_0220-M.jpg" alt="could not load"/>
+const img9 = <img src="https://photos.smugmug.com/1x1-photos/i-HBPkJgv/0/de57fa68/M/OGD_0384-M.jpg" alt="could not load"/>
+const img10 = <img src="https://photos.smugmug.com/1x1-photos/i-6FzCVjr/0/90632e7f/M/OGD_0421-M.jpg" alt="could not load"/>
+
+const img11 = <img src="https://photos.smugmug.com/1x1-photos/i-4xg32ZM/0/b2c6359f/M/OGD_0441-M.jpg" alt="could not load"/>
+const img12 = <img src="https://photos.smugmug.com/1x1-photos/i-3j4ZJbK/0/b8733b9a/M/OGD_0505-M.jpg" alt="could not load"/>
+const img13 = <img src="https://photos.smugmug.com/1x1-photos/i-WJgnsFt/0/894fa7ea/M/OGD_0661-M.jpg" alt="could not load"/>
+const img14 = <img src="https://photos.smugmug.com/1x1-photos/i-RFKZVG6/0/ad3dd615/M/OGD_0667-M.jpg" alt="could not load"/>
+const img15 = <img src="https://photos.smugmug.com/1x1-photos/i-cRsNtjZ/0/f5a7326f/M/OGD_0746-M.jpg" alt="could not load"/>
+
+const img16 = <img src="https://photos.smugmug.com/1x1-photos/i-gS99Psz/0/c92afd15/M/OGD_0973-M.jpg" alt="could not load"/>
+const img17 = <img src="https://photos.smugmug.com/1x1-photos/i-RNGhfFf/0/9842c83a/M/OGD_0136-M.jpg" alt="could not load"/>
+const img18 = <img src="https://photos.smugmug.com/1x1-photos/i-BjdHqBN/0/d7e06a3e/M/OGD_0245-M.jpg" alt="could not load"/>
+const img19 = <img src="https://photos.smugmug.com/1x1-photos/i-dfHGLp6/0/927f4d52/M/OGD_0295-M.jpg" alt="could not load"/>
+const img20 = <img src="https://photos.smugmug.com/1x1-photos/i-4KLRMvr/0/9014fa27/M/OGD_3116-M.jpg" alt="could not load"/>
+
+const img21 = <img src="https://photos.smugmug.com/1x1-photos/i-4sJmrTM/0/160c365f/M/OGD_4061-M.jpg" alt="could not load"/>
+const img22 = <img src="https://photos.smugmug.com/1x1-photos/i-h46XV74/0/7a21a08f/M/OGD_0256-M.jpg" alt="could not load"/>
+const img23 = <img src="https://photos.smugmug.com/1x1-photos/i-cJJGDMm/0/c372e35c/M/ODG_1150-M.jpg" alt="could not load"/>
+const img24 = <img src="https://photos.smugmug.com/1x1-photos/i-m5H66BG/0/3db3e07a/M/ODG_1295-M.jpg" alt="could not load"/>
+const img25 = <img src="https://photos.smugmug.com/1x1-photos/i-rxSNtbs/0/794989eb/M/ODG_1314-M.jpg" alt="could not load"/>
+
+const img26 = <img src="https://photos.smugmug.com/1x1-photos/i-BLz4B43/0/99619386/M/ODG_1355-M.jpg" alt="could not load"/>
+const img27 = <img src="https://photos.smugmug.com/1x1-photos/i-LcCvw9Q/0/e21bc6a5/M/ODG_1448-M.jpg" alt="could not load"/>
+const img28 = <img src="https://photos.smugmug.com/1x1-photos/i-NXH3Fkk/0/2a1803ab/M/ODG_1741-M.jpg" alt="could not load"/>
+const img29 = <img src="https://photos.smugmug.com/1x1-photos/i-Mjdtj58/0/766de235/M/ODG_1786-M.jpg" alt="could not load"/>
+const img30 = <img src="https://photos.smugmug.com/1x1-photos/i-w9Mzvns/0/5f5b42b0/M/ODG_1895-M.jpg" alt="could not load"/>
+const img31 =  <img src="https://photos.smugmug.com/1x1-photos/i-j2cQSJ9/0/8922e31a/L/ODG_1934-L.jpg" alt="could not load"/>
+const img32 =  <img src="https://photos.smugmug.com/1x1-photos/i-Msr4QXv/0/596ab209/L/OGD_0152-L.jpg" alt="could not load"/>
+
+let imgArray = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15, img16, 
+    img17, img18, img19, img20, img21, img22, img23, img24, img25, img26, img27, img28, img29, img30, img31, img32, 
+    img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15, img16, 
+    img17, img18, img19, img20, img21, img22, img23, img24, img25, img26, img27, img28, img29, img30, img31, img32]
+
+    export default imgArray
